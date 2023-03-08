441a91d4ac3fbf81951e1d2cbfaed1def58f898b
Полина Галкина <polygal@inbox.ru>
2023-03-08 13:44:32 +0300

    ex25

--- ex25/Program.cs
+++ ex25/Program.cs
@@ -1,1 +1,33 @@
 ﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.Clear();
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string RealInput = System.Console.ReadLine();
+    int result = int.Parse(RealInput);
+    return result;
+}
+int Power(int powerBase, int exponent)
+{
+    int power = 1;
+    for (int i = 0; i < exponent; i++)
+    {
+        power *= powerBase;
+    }
+    return power;
+}
+bool ValidateExponent(int exponent)
+{
+    if (exponent < 0)
+    {
+        System.Console.WriteLine("Exponent cannot be less than zero");
+        return false;
+    }
+    return true;
+}
+int powerBase = Prompt("Enter number: ");
+int exponent = Prompt("Enter exponent: ");
+if(ValidateExponent(exponent))
+{
+    System.Console.WriteLine($"Number {powerBase}  to the power {exponent} equals {Power(powerBase, exponent)}");
+}
