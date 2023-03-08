66bbf3fd657be7b148f16edb3a980c071719ffb8
Полина Галкина <polygal@inbox.ru>
2023-03-08 13:56:52 +0300

    ex27

---
+++ ex27/Program.cs
@@ -0,0 +1,22 @@
+﻿//  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.Clear();
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string RealInput = System.Console.ReadLine();
+    int result = int.Parse(RealInput);
+    return result;
+}
+int Sum(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result += number % 10;
+        number = number / 10;
+    }
+    return result;
+}
+
+int number = Prompt("Enter number: ");
+ System.Console.WriteLine($"Sum of all digits in number {number} is {Sum(number)}");
